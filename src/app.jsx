f3fbdd9851dc4c2e9ac112b60628e70fb7582969
bwaibel <bryn.waibel@gmail.com>
2015-07-21 08:45:09 -0700

    add a basic mixin based grid from http://webdesign.tutsplus.com/tutorials/a-simple-mixin-alternative-to-standard-css-grids--webdesign-16958

--- src/app.jsx
+++ src/app.jsx
@@ -1,6 +1,43 @@
 import React from 'react';
 
 React.render(
-  <div>Hello World!</div>,
+  (<div><header>
+    <h1>Site Title</h1>
+</header>
+<main>
+    <section>
+        <h1>Latest Articles</h1>
+        <article>
+            <h1>Article Title</h1>
+            <p>...</p>
+        </article>
+        <article>
+            <h1>Article Title</h1>
+            <p>...</p>
+        </article>
+        <article>
+            <h1>Article Title</h1>
+            <p>...</p>
+        </article>
+        <article>
+            <h1>Article Title</h1>
+            <p>...</p>
+        </article>
+        <article>
+            <h1>Article Title</h1>
+            <p>...</p>
+        </article>
+        <article>
+            <h1>Article Title</h1>
+            <p>...</p>
+        </article>
+    </section>
+    <aside>
+        <p>...</p>
+    </aside>
+</main>
+<footer>
+    <p>Example Footer</p>
+</footer></div>),
   document.getElementById('app')
 );
