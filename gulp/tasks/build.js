d3aaf51414e2cb7871290137758d694e018f314d
bwaibel <bryn.waibel@gmail.com>
2015-07-20 23:45:45 -0700

    add a dev server, split up the build steps

--- gulp/tasks/build.js
+++ gulp/tasks/build.js
@@ -4,21 +4,27 @@ var babelify = require('babelify');
 var source = require('vinyl-source-stream');
 var less = require('gulp-less');
 
-gulp.task('build', function () {
-  browserify({
-    entries: 'src/app.jsx',
-    extensions: ['.jsx'],
-    debug: true
-  })
-  .transform(babelify)
-  .bundle()
-  .pipe(source('app.js'))
-  .pipe(gulp.dest('dist'));
+gulp.task('jsx', function() {
+  return browserify({
+      entries: 'src/app.jsx',
+      extensions: ['.jsx'],
+      debug: true
+    })
+    .transform(babelify)
+    .bundle()
+    .pipe(source('app.js'))
+    .pipe(gulp.dest('dist'));
+});
 
-  gulp.src('src/index.html')
-   .pipe(gulp.dest('dist'));
+gulp.task('html', function () {
+  return gulp.src('src/index.html')
+    .pipe(gulp.dest('dist'));
+});
 
-  gulp.src('src/less/app.less')
+gulp.task('less', function() {
+  return gulp.src('src/less/app.less')
     .pipe(less())
     .pipe(gulp.dest('dist'));
 });
+
+gulp.task('build', ['jsx','html','less']);
